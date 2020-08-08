77fa91a2f2941b2fef14dca5e4223362677dbe53
nawan44 <rachmat.d.gunawan@gmail.com>
2020-08-09 00:14:49 +0700

    insert data

--- create_Database.sql
+++ create_Database.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE indonesia;
+
+
+---POSTGRESQL
+CREATE DATABASE "Indonesia"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
---
+++ insert_Data.sql
@@ -0,0 +1,8 @@
+    --ACEH
+    INSERT INTO public.province(
+	provinceid, provincename, capitalcityname, pdb, area, totalpopulation, web)
+	VALUES (11, 'Aceh', 'Banda Aceh', 17124000, 57365, 5189466, 'http://www.acehprov.go.id/');
+    --SUMATERA UTARA
+    INSERT INTO public.province(
+	provinceid, provincename, capitalcityname, pdb, area, totalpopulation, web)
+	VALUES (11, 'Sumatera Utara', 'Medan', '-', 72981, 12982204, 'http://www.sumutprov.go.id/');
