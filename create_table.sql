dee9f671322de12904a6a6d156f038ddb0a90478
nawan44 <rachmat.d.gunawan@gmail.com>
2020-08-08 11:27:17 +0700

    create table

---
+++ create_table.sql
@@ -0,0 +1,13 @@
+--create table without condition
+CREATE TABLE Province (
+    ProvinceID int,
+    ProvinceName varchar(255),
+    CapitalCityName varchar(255),
+    PDB varchar(255),
+    Area varchar(255),
+    TotalPopulation varchar(255),
+    Web varchar(255)
+
+);
+
+
